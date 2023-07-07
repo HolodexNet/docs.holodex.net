cb5ede74437c7872cd4d18677f988a81ef7f9e2d
David Chen <3145205+RiceCakess@users.noreply.github.com>
2023-07-06 22:54:51 -0700

    Update cli pkg

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle openapi/holodex_v2.yaml && \
+npx npx @redocly/cli build-docs openapi/holodex_v2.yaml && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
